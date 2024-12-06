be0c29dd8ea948f0619e90f9787321d598ca7cdf
dugalcedo <dougalcedo@gmail.com>
2024-12-06 11:07:12 +0100

    pages folder

---
+++ src/pages/CalculatorPage.jsx
@@ -0,0 +1,20 @@
+const buttons = {
+    number: [
+        
+    ]
+}
+
+const CalculatorPage = () => {
+    return (
+        <div>
+            <h2>Calculator</h2>
+            <div id="calculator-container">
+                <div id="calculator-buttons">
+                    <div id="calculator-numbers">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
